44fed44d644aa97d9b77d1ca0a311b34d93077bd
“Swapnesh <swapneshsangle1@gmail.com>
2022-05-14 13:10:33 +0530

    test: it will add n number of elements in string

--- src/StringCalculator.spec.js
+++ src/StringCalculator.spec.js
@@ -13,4 +13,12 @@ describe('String Calculator', () => {
   it('will add and return value for 2 elements in string', () => {
     assert.strictEqual(addString('1,2'), 3);
   });
+
+  it('will add and return value for n elements in string', () => {
+    const randomInput = Array.from({ length: 20 }, () =>
+      Math.floor(Math.random() * 20)
+    );
+    const sum = randomInput.reduce((accu, val) => accu + val, 0);
+    assert.strictEqual(addString(randomInput.join(',')), sum);
+  });
 });
