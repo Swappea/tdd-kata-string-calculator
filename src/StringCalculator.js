60447be91758f026dc65cdfd56cded06f669e8f1
“Swapnesh <swapneshsangle1@gmail.com>
2022-05-14 13:02:07 +0530

    test: addition of 1 element of string

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,9 +1,13 @@
-const addString = inputString => {
+const addString = (inputString) => {
   if (!inputString) {
-    return 0
+    return 0;
   }
-}
+
+  if (String(inputString).length === 1) {
+    return +inputString;
+  }
+};
 
 module.exports = {
-  addString
-}
+  addString,
+};
--- src/StringCalculator.spec.js
+++ src/StringCalculator.spec.js
@@ -3,6 +3,10 @@ const { addString } = require('./StringCalculator')
 
 describe('String Calculator', () => {
   it('will add and return 0 for empty string', () => {
-    assert.strictEqual(addString(''), 0)
-  })
-})
+    assert.strictEqual(addString(''), 0);
+  });
+
+  it('will add and return value for 1 element in string', () => {
+    assert.strictEqual(addString('1'), 1);
+  });
+});
