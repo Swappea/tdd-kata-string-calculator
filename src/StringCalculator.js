1d5e408d93ebcc82e4324ab9f9b9c8a6fb09f57b
“Swapnesh <swapneshsangle1@gmail.com>
2022-05-14 13:30:57 +0530

    refactor: move delimiters to variable

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -7,14 +7,17 @@ const addString = (inputString) => {
     return +inputString;
   }
 
-  const delimitedArray = inputString.split('\n');
+  const delimiter = ',';
+  const newLineDelimter = '\n';
+
+  const delimitedArray = inputString.split(newLineDelimter);
   let sum = 0;
   for (const line of delimitedArray) {
-    if (line.endsWith(',')) {
+    if (line.endsWith(delimiter)) {
       throw new Error('SyntaxError');
     }
 
-    const delimitedValues = line.split(',');
+    const delimitedValues = line.split(delimiter);
 
     for (const value of delimitedValues) {
       sum += Number(value);
